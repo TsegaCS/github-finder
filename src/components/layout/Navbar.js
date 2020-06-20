ef60335062a8004a0869a80a6382e9bcc7b037ed
TsegaCS <tsegupower@gmail.com>
2020-06-20 16:41:27 +0300

    Added States and Props && Restructuring by Destructring

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Profiler",
-    // add iconprops here
-  };
+const Navbar = ({ title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>{title}</h1>
+    </nav>
+  );
+};
+Navbar.defaultProps = {
+  title: "Github Profiler",
+  // add iconprops here
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>{this.props.title}</h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Navbar;
